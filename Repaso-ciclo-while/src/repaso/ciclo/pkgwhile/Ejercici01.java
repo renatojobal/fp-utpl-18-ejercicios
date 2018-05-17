3a74785ebae8d0842cbc3ce83983c16b24c535f8
Renato Balcazar <renatojbl99@gmail.com>
2018-05-16 23:15:33 -0500

    Ejercicios resueltos

--- Repaso-ciclo-while/src/repaso/ciclo/pkgwhile/Ejercici01.java
+++ Repaso-ciclo-while/src/repaso/ciclo/pkgwhile/Ejercici01.java
@@ -11,10 +11,16 @@ public class Ejercici01 {
     public static void main(String[] args) {
         
         // Declaración de varibales        
-        String cadenaEncabezado = "", cadenaArticulos = "";
+        String cadenaEncabezado = "", cadenaArticulos = "", articulo = "";
         String nombre = "";
-        Double subtotal, iva,total,suma=null;
+        Double subtotal, iva,total, precioUnit, precioTotal, cantidad;
+        subtotal = 0.0;
         int continuar;
+
+
+            
+        
+        
         cadenaEncabezado = "\t\tFactura\n\nSupermercado: 'Todo a menor precio'\nNombre del Cliente: ";
         cadenaArticulos = "Articulo\t\tCantidad\t\tPrecio Unitario\t\tPrecio Total\n";
         
@@ -25,31 +31,27 @@ public class Ejercici01 {
         nombre = entrada.nextLine();
         
         // Concatenamos el nombre a cadenaEncabezado
-        //cadenaEncabezado = ("%s%s", cadenaEncabezado, nombre);
+        cadenaEncabezado = String.format("%s%s\n", cadenaEncabezado, nombre);
         
         
         // Ingreso de datos de los artículos
         continuar = 1;
         while (continuar == 1) {
             // Delcaramos variables 
-            String articulo = "";
-            Double precioUnit, precioTotal;
-            
-            int cantidad;
-            
+            entrada.nextLine();
             System.out.println("Ingrese el nombre del articulo: ");
             articulo = entrada.nextLine();
             System.out.println("Ingrese la cantidad: ");
-            cantidad = entrada.nextInt();
+            cantidad = entrada.nextDouble();
             System.out.println("Ingrese el precio unitario del articulo: ");
             precioUnit = entrada.nextDouble();
-            
+            entrada.nextLine();
             
             precioTotal = cantidad*precioUnit;
-            suma = suma + precioTotal;
+            subtotal = subtotal + precioTotal;
             
             // Concatenación de cadena
-            //cadenaArticulos = ("%s%s\t\t%d\t\t%f\t\t%.2f\n", cadenaEncabezado, articulo, cantidad, precioUnit, precioTotal);
+            cadenaArticulos = String.format("%s%s\t\t%02.0f\t\t\t%.2f\t\t\t%.2f\n", cadenaArticulos, articulo, cantidad, precioUnit, precioTotal);
             
             // Continuar o no el cilo
             System.out.println("\nDesea ingresar otro artículo?: \n 1 = 'Si'  2 = 'No, salir'");
@@ -58,11 +60,11 @@ public class Ejercici01 {
         }
         
         // Calculo del subottaol, iva y total
-        subtotal = suma;
+        
         iva = subtotal * 0.14;
         total = subtotal - iva;
         // Concatenación de cadena
-        cadenaArticulos = ("%s\t\t\t\t%.2f\t\t%.2f\n%.2f\t\t%.2f\n%.2f\t\t%.2f\n", cadenaEncabezado, subtotal, iva, total);
+        cadenaArticulos = String.format("%s\t\t\t\t\t\t%s\t\t%.2f\n\t\t\t\t\t\t%s\t\t\t%.2f\n\t\t\t\t\t\t%s\t\t\t%.2f\n", cadenaArticulos, "Subtotal", subtotal, "Iva 14%", iva, "Total", total);
         
         // Salida de datos
        
--- Repaso-ciclo-while/src/repaso/ciclo/pkgwhile/Ejercicio02.java
+++ Repaso-ciclo-while/src/repaso/ciclo/pkgwhile/Ejercicio02.java
@@ -3,20 +3,81 @@ package repaso.ciclo.pkgwhile;
 import java.util.Scanner;
 
 /**
- *
+ * Ejercicio 2
  * @author Renato
  */
 public class Ejercicio02 {
     public static void main(String[] args) {
         
         // Declaración de varibales        
+        int contador = 1, limite, contArgentina=0, contChile=0, contColombia=0, contResto=0;
+        Double sumImpuestos=0.0, sumPrecioVenta=0.0, costo=0.0, impuesto=0.0, precioVenta=0.0; 
+        String cadena01, cadena02, marca, origen;
         
+        cadena01 = "\tReporte de Autos Importados\n\nMarca\t\t\tOrigen\t\tCosto\t\tImpuesto\t\tPrecio Venta\n";
+        cadena02 = "";
         
         // Ingreso de datos
         Scanner entrada = new Scanner(System.in);
         
-        // Salida de datos
+        System.out.println("Ingrese el número de autos a ingresar: ");
+        limite = entrada.nextInt();
+        while (contador <= limite) {
+            System.out.printf("\n**********Vehiculo No. %d**********\n", contador);
+            // MARCA
+            entrada.nextLine();
+            System.out.println("Ingrese la MARCA del vehiculo: ");
+            marca = entrada.nextLine();
+            
+            // ORIGEN
+            System.out.println("Ingrese el ORIGEN del vehiculo: ");
+            origen = entrada.nextLine();
+             
+            // COSTO
+            System.out.println("Ingrese el COSTO del vehiculo: ");
+            costo = entrada.nextDouble();
+            
+            // IMPUESTO
+            switch (origen) {
+                case "Argentina":
+                    impuesto = costo*0.20;
+                    contArgentina = contArgentina + 1;
+                    break;
+                case "Colombia":
+                    impuesto = costo*0.25;
+                    contColombia = contColombia + 1;
+                    break;
+                case "Chile":
+                    impuesto = costo*0.30;
+                    contChile = contChile + 1;
+                    break;
+                default:
+                    impuesto = costo*0.40;
+                    contResto = contResto + 1;
+                    break;
+            }
+            
+            sumImpuestos = sumImpuestos + impuesto;
+            
+            // PRECIO VENTA
+            precioVenta = costo + impuesto;
+            
+            // Concatenación de cadena01
+            cadena01 = String.format("%s%s\t\t\t%s\t\t%.2f\t\t%.0f\t\t%.0f\n", cadena01, marca, origen, costo, impuesto, precioVenta);
+            
+            
+            // Concatenacion cadena02
+            cadena02 = String.format("\nTotal Autos\t%d\tTot.Imps %.0f\tTot. PV\t%.0f\n\nArgentina\t\t%d\nChile\t\t\t%d\nColombia\t\t%d\nResto de paises\t\t%d",
+                    contador, sumImpuestos, sumPrecioVenta, contArgentina, contChile, contColombia, contResto);
+            
+            // Suma de contador
+            contador = contador + 1;
+        }
         
+        
+        // Salida de datos
+        System.out.println(cadena01);
+        System.out.println(cadena02);
         
     }
 }
